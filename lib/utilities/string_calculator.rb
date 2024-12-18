113aa616af267d8abc97aa4e53b8585a4215292e
Shivam Kolli <ishivamkolli@gmail.com>
2024-12-18 14:10:08 +0530

    added logic to return multiplication of string integers

--- lib/utilities/string_calculator.rb
+++ lib/utilities/string_calculator.rb
@@ -8,16 +8,26 @@ module Utilities
       def add(input)
         return 0 if input.empty?
 
-        values = extract_values(input)
-        raise_error_if_negative_integer(values)
-
-        addition_of_valid_integers(values)
+        result_from_extracted_values(input)
       end
 
       private
 
+      def result_from_extracted_values(input)
+        delimiter_section, number_section = input.split("\n")
+        delimiter = delimiter_section.split('//').last
+
+        if delimiter.include?('*')
+          integers = extract_values(number_section)
+          multiplication_of_valid_integers(integers)
+        else
+          integers = extract_values(input)
+          addition_of_valid_integers(integers)
+        end
+      end
+
       def extract_values(input)
-        input.split(/[^0-9-]+/).map(&:to_i)
+        input.split(/[^0-9-]+/).reject(&:empty?).map(&:to_i)
       end
 
       def raise_error_if_negative_integer(array)
@@ -27,10 +37,22 @@ module Utilities
         raise "negative numbers not allowed - (#{negatives.join(', ')})"
       end
 
-      def addition_of_valid_integers(array)
-        valid_integers = array.select { |i| i < NUMBER_TOO_BIG }
+      def valid_integers(array)
+        raise_error_if_negative_integer(array)
 
-        valid_integers.sum
+        array.select { |i| i < NUMBER_TOO_BIG }
+      end
+
+      def addition_of_valid_integers(array)
+        integers = valid_integers(array)
+
+        integers.sum
+      end
+
+      def multiplication_of_valid_integers(array)
+        integers = valid_integers(array)
+
+        integers.reduce(:*)
       end
     end
   end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,90 +5,94 @@ require 'utilities/string_calculator'
 RSpec.describe Utilities::StringCalculator do
   describe 'add' do
     it 'returns 0 for empty string' do
-      expect(Utilities::StringCalculator.add('')).to eq(0)
+      expect(described_class.add('')).to eq(0)
     end
 
     context 'single number' do
       it "returns 1 for '1'" do
-        expect(Utilities::StringCalculator.add('1')).to eq(1)
+        expect(described_class.add('1')).to eq(1)
       end
 
       it "returns 2 for '2'" do
-        expect(Utilities::StringCalculator.add('2')).to eq(2)
+        expect(described_class.add('2')).to eq(2)
       end
     end
 
     context 'two numbers' do
       it "returns 3 for the string '1,2'" do
-        expect(Utilities::StringCalculator.add('1,2')).to eq(3)
+        expect(described_class.add('1,2')).to eq(3)
       end
 
       it "returns 1 for the string '0, 1'" do
-        expect(Utilities::StringCalculator.add('0,1')).to eq(1)
+        expect(described_class.add('0,1')).to eq(1)
       end
 
       it "returns 3 for the string '3, 0'" do
-        expect(Utilities::StringCalculator.add('3,0')).to eq(3)
+        expect(described_class.add('3,0')).to eq(3)
       end
 
       it "returns 200 for the string '190, 10'" do
-        expect(Utilities::StringCalculator.add('190,10')).to eq(200)
+        expect(described_class.add('190,10')).to eq(200)
       end
 
       it "returns 328 for the string '78, 250'" do
-        expect(Utilities::StringCalculator.add('78,250')).to eq(328)
+        expect(described_class.add('78,250')).to eq(328)
       end
     end
 
     context 'ignore numbers greater than 1000' do
       it "returns 3 for the string '1,2,1000'" do
-        expect(Utilities::StringCalculator.add('1,2,1000')).to eq(3)
+        expect(described_class.add('1,2,1000')).to eq(3)
       end
 
       it "returns 0 for the string '1000,1500'" do
-        expect(Utilities::StringCalculator.add('1000,1500')).to eq(0)
+        expect(described_class.add('1000,1500')).to eq(0)
       end
 
       it "returns 1000 for the string '1,999,1000'" do
-        expect(Utilities::StringCalculator.add('1,999,1000')).to eq(1000)
+        expect(described_class.add('1,999,1000')).to eq(1000)
       end
     end
 
     context 'negative numbers' do
       it 'raises exception when string contains a negative number' do
-        expect { Utilities::StringCalculator.add('-1') }.to raise_error('negative numbers not allowed - (-1)')
+        expect { described_class.add('-1') }.to raise_error('negative numbers not allowed - (-1)')
       end
 
       it 'raises exception when string contains single negative number' do
-        expect { Utilities::StringCalculator.add('2, -1') }.to raise_error('negative numbers not allowed - (-1)')
+        expect { described_class.add('2, -1') }.to raise_error('negative numbers not allowed - (-1)')
       end
 
       it 'raises exception when string contains multiple negative numbers' do
         expect do
-          Utilities::StringCalculator.add('2, -1, -30')
+          described_class.add('2, -1, -30')
         end.to raise_error('negative numbers not allowed - (-1, -30)')
       end
     end
 
     context 'delimiters' do
       it "returns 3 for the string '//;\n1;2'" do
-        expect(Utilities::StringCalculator.add("//;\n1;2")).to eq(3)
+        expect(described_class.add("//;\n1;2")).to eq(3)
       end
 
       it "returns 6 for the string '//;\n1;2;3,'" do
-        expect(Utilities::StringCalculator.add("//;\n1;2;3")).to eq(6)
+        expect(described_class.add("//;\n1;2;3")).to eq(6)
       end
 
       it "returns 8 for the string '3\n2\n3, 0'" do
-        expect(Utilities::StringCalculator.add("//;3\n2;\n3, 0")).to eq(8)
+        expect(described_class.add("//;3\n2;\n3, 0")).to eq(8)
       end
 
       it "returns 6 for the string '//[***]\n1***2***3'" do
-        expect(Utilities::StringCalculator.add("//[***]\n1***2***3")).to eq(6)
+        expect(described_class.add("//[***]\n1***2***3")).to eq(6)
       end
 
       it "returns 6 for the string '//[*][%]\n1*2%3'" do
-        expect(Utilities::StringCalculator.add("//[*][%]\n1*2%3")).to eq(6)
+        expect(described_class.add("//[*][%]\n1*2%3")).to eq(6)
+      end
+
+      it "returns 12 for the string '//*\n1*4*3'" do
+        expect(described_class.add("//*\n1*4*3")).to eq(12)
       end
     end
   end
